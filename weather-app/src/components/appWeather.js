39e7f30c2f4641dd0af9e6ab238527bf8e9a2b5c
elaylevy123 <elaylevi2@gmail.com>
2025-03-18 00:17:48 +0200

    commit2

--- weather-app/src/components/appWeather.js
+++ weather-app/src/components/appWeather.js
@@ -1,12 +1,29 @@
-import React from "react";
-import WeatherForm from './weahterForm'
+import React,{ useEffect, useState} from 'react';
+import axios from 'axios';
+import WeahterForm from './weahterForm'
 import WeatherInfo from './weahterInfo'
+import {useSearchParams} from "react-router-dom";
 
 export default function AppWeather() {
-    return(
+    let [info,setInfo] = useState({});
+    const [query] = useSearchParams();
+    useEffect(() => {
+        doApi(query.get("q"))
+    },[query]);
+
+    const doApi = async(_town) => {
+        let url = `http://api.openweathermap.org/data/2.5/weather?q=${_town}&APPID=333bfa4905ca5f50692d239f449ef45b&units=metric`;
+        let resp = await axios.get(url);
+        console.log(resp.data);
+        setInfo(resp.data)
+
+    }
+
+
+    return (
         <React.Fragment>
-            <WeatherForm />
-            <WeatherInfo />
+            <WeahterForm doApi={doApi} />
+            {info.name && <WeatherInfo info={info} />}
         </React.Fragment>
     )
 }
--- weather-app/src/components/weahterForm.js
+++ weather-app/src/components/weahterForm.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useRef } from 'react'
+import {useNavigate} from "react-router-dom";
 
-export default function WeatherForm() {
+
+export default function WeahterForm(props) {
+    const nav = useNavigate();
+    const inputRef = useRef();
+
+    const onSearchClick = () => {
+        props.doApi(inputRef.current.value);
+    }
+
     return (
         <nav className="container-fluid bg-warning p-2">
             <div className="container">
-              <div className="row">
-                <div className="logo col-auto text-light">
-                    <h2>weather</h2>
+                <div className="row">
+                    <div className="logo col-auto text-light">
+                        <h2>Weather</h2>
+                    </div>
+                    <div className="d-flex col-md-4">
+                        <input onkeydown={(e) => {
+                            if (e.key == "Enter"){onSearchClick()}
+                        }}
+                            ref={inputRef} id="id_input" placeholder="search for town weather..." type="search" className="form-control" />
+                        <button onClick={onSearchClick} id="search_btn" className="btn btn-dark">Search</button>
+                    </div>
                 </div>
-                  <div className="d-flex col-md-4">
-                      <input id="id_input" placeholder="search for town weather.." type="search"
-                             className="form-control" />
-                      <button id="search_btn" className="btn btn-dark">Search</button>
-                  </div>
-              </div>
             </div>
         </nav>
     )
